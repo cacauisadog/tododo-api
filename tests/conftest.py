b3e4269175724d3166459ec571525a4f12f0bc72
cacauisadog <cbcaue@protonmail.com>
2021-02-20 17:07:03 -0300

    refactor user tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,19 @@
 import pytest
+import asyncio
 
 from typing import Generator
 
 from starlette.testclient import TestClient
 from tortoise.contrib.test import initializer, finalizer
 
+from app.models import Users
 from app.config.settings import Settings, get_settings
 from app.main import create_application
 
 settings: Settings = get_settings()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def client() -> Generator:
     # set up
     app = create_application()
@@ -26,6 +28,15 @@ def client() -> Generator:
     finalizer()
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def event_loop(client: TestClient) -> Generator:
     yield client.task.get_loop()
+
+
+@pytest.fixture()
+def user_joe(event_loop: asyncio.AbstractEventLoop) -> Users:
+    async def create_user():
+        user = await Users.create(email='joe@tododo.com', password='safepassword')
+        return user
+
+    return event_loop.run_until_complete(create_user())
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,30 +1,15 @@
-from app.models import Users
 import asyncio
+import pytest
+import tortoise
 
 from fastapi.testclient import TestClient
 
+from app.models import Users
 
-def test_user_crud_operations(client: TestClient, event_loop: asyncio.AbstractEventLoop):
+
+def test_create_user(client: TestClient, event_loop: asyncio.AbstractEventLoop):
     payload = {'email': 'admin@tododo.com', 'password': 'safepassword'}
 
-    assert _get_all_users(client) == []
-    created_user_obj = _create_user(client, event_loop, payload)
-    created_user_dict = _get_user(client, created_user_obj.id)
-    assert created_user_dict in _get_all_users(client)
-
-    update_payload = {'email': 'user@tododo.com'}
-    updated_user_dict = _update_user(client, created_user_dict, update_payload)
-    assert updated_user_dict in _get_all_users(client)
-
-    _delete_user(client, updated_user_dict['id'])
-
-    assert _get_all_users(client) == []
-    _get_user(client, created_user_obj.id, expected_status_code=404)
-    _update_user(client, created_user_dict, update_payload, expected_status_code=404)
-    _delete_user(client, updated_user_dict['id'], expected_status_code=404)
-
-
-def _create_user(client: TestClient, event_loop: asyncio.AbstractEventLoop, payload: dict):
     response = client.post('/users', json=payload)
     assert response.status_code == 200, response.text
     data = response.json()
@@ -32,50 +17,68 @@ def _create_user(client: TestClient, event_loop: asyncio.AbstractEventLoop, payl
     assert 'id' in data
     user_id = data['id']
 
-    user_obj = event_loop.run_until_complete(_get_user_by_db(user_id))
+    user_obj = event_loop.run_until_complete(_get_user_from_db(user_id))
     assert user_obj.id == user_id
 
-    return user_obj
 
-
-def _get_all_users(client: TestClient):
+def test_get_all_users_empty(client: TestClient):
     response = client.get('/users')
     assert response.status_code == 200, response.text
-    return response.json()
+    data = response.json()
+    assert data == []
 
 
-def _get_user(client: TestClient, user_id: int, expected_status_code: int = 200):
-    response = client.get(f'/users/{user_id}')
-    assert response.status_code == expected_status_code, response.text
-
-    if expected_status_code == 200:
-        user_json = response.json()
-        assert user_json['id'] == user_id
-
-        return user_json
+def test_get_all_users(client: TestClient, user_joe: Users):
+    response = client.get('/users')
+    assert response.status_code == 200, response.text
+    joe_json = user_joe.to_pydantic().json(separators=(',', ':'))
+    assert joe_json in response.text
 
 
-def _update_user(client: TestClient, user_dict: dict, update_payload: dict, expected_status_code: int = 200):
-    response = client.put(f'/users/{user_dict["id"]}', json=update_payload)
-    assert response.status_code == expected_status_code, response.text
+def test_update_user(client: TestClient, event_loop: asyncio.AbstractEventLoop, user_joe: Users):
+    payload = {'email': 'superjoe@tododo.com', 'password': 'evensaferpassword'}
 
-    if expected_status_code == 200:
-        updated_user_json = response.json()
-        assert updated_user_json['id'] == user_dict['id']
-        assert updated_user_json['email'] == update_payload['email']
+    response = client.put(f'/users/{user_joe.id}', json=payload)
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['email'] == payload['email']
 
-        return updated_user_json
-
-
-def _delete_user(client: TestClient, user_id: int, expected_status_code=200):
-    response = client.delete(f'/users/{user_id}')
-    assert response.status_code == expected_status_code, response.text
-
-    if expected_status_code == 200:
-        res = response.json()
-        assert res['message'] == f'Deleted user {user_id}'
+    joe = event_loop.run_until_complete(_get_user_from_db(user_joe.id))
+    assert joe.email == payload['email']
+    assert joe.password != user_joe.password
 
 
-async def _get_user_by_db(user_id):
+def test_delete_user(client: TestClient, event_loop: asyncio.AbstractEventLoop, user_joe: Users):
+    response = client.delete(f'/users/{user_joe.id}')
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['message'] == f'Deleted user {user_joe.id}'
+
+    with pytest.raises(tortoise.exceptions.DoesNotExist):
+        event_loop.run_until_complete(_get_user_from_db(user_joe.id))
+
+
+def test_get_nonexistant_user(client: TestClient):
+    response = client.get('/users/66')
+    assert response.status_code == 404, response.text
+    data = response.json()
+    assert data['detail'] == 'User not found.'
+
+
+def test_update_nonexistant_user(client: TestClient):
+    response = client.put('/users/66', json={'password': 'whateveritwontworkanyway'})
+    assert response.status_code == 404, response.text
+    data = response.json()
+    assert data['detail'] == 'User not found.'
+
+
+def test_delete_nonexistant_user(client: TestClient):
+    response = client.delete('/users/66')
+    assert response.status_code == 404, response.text
+    data = response.json()
+    assert data['detail'] == 'User not found.'
+
+
+async def _get_user_from_db(user_id):
     user = await Users.get(id=user_id)
     return user
